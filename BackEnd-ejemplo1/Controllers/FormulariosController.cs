0307bbbce51f58984169acf73927add7478b0759
DiegoRamSam <ajionity@gmai.com>
2022-10-31 10:05:50 -0600

    Agregar archivos de proyecto.

---
+++ BackEnd-ejemplo1/Controllers/FormulariosController.cs
@@ -0,0 +1,26 @@
+﻿using BackEnd_ejemplo1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEnd_ejemplo1.Controllers
+{
+    public class FormulariosController : Controller
+    {
+        // GET: Formularios
+        public ActionResult Index()
+        {
+            trabajador obj = new trabajador();
+
+            return View(obj);
+        }
+        public ActionResult Index2()
+        {
+            trabajador obj =  new trabajador();
+
+            return View(obj);
+        }
+    }
+}
---
+++ BackEnd-ejemplo1/Models/Coche.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_ejemplo1.Models
+{
+    public class Coche
+    {
+        public string IdCoche { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Propietario { get; set; }
+
+        public Coche(string idCoche, string marca, string modelo, string propietario)
+        {
+            this.IdCoche = idCoche;
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Propietario = propietario;
+        }
+    }
+}
